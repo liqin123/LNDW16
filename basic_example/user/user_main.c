75b3eb85e628582cbc55a65c09304a2a8476e920
Yannic Ahrens <yannic.ahrens@mailbox.tu-dresden.de>
2016-05-12 10:16:50 +0200

    Set a new MAC

--- basic_example/user/user_main.c
+++ basic_example/user/user_main.c
@@ -11,7 +11,8 @@ os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void loop(os_event_t *events);
 
 #define MAC_SIZE 6
-uint8 mac_addr [MAC_SIZE] = {0, 0, 0, 0, 0, 0};
+uint8 original_mac_addr [MAC_SIZE] = {0, 0, 0, 0, 0, 0};
+uint8 new_mac_addr[MAC_SIZE] = {0x00, 0x11, 0x22, 0x33, 0x44, 0x55};
 
 // wifi: ESP has two "interfaces": one when acting as a station and another when it's acting as an AP
 
@@ -20,10 +21,19 @@ uint8 mac_addr [MAC_SIZE] = {0, 0, 0, 0, 0, 0};
 static void ICACHE_FLASH_ATTR
 loop(os_event_t *events)
 {
+    // clearing new_mac_addr so we'll get fresh values
+    memset(new_mac_addr, 0, MAC_SIZE);
     int i;
-    os_printf("\n MAC: ");
+    os_printf("\n Original MAC: ");
     for (i=0; i < MAC_SIZE; ++i) {
-        os_printf("%x", mac_addr[i]);
+        os_printf("%x", original_mac_addr[i]);
+    }
+    if (wifi_get_macaddr(STATION_IF, new_mac_addr) != true) {
+        os_printf("Failed to get the new MAC address\n");
+    } 
+    os_printf("\n New MAC: ");
+    for (i=0; i < MAC_SIZE; ++i) {
+        os_printf("%x", new_mac_addr[i]);
     }
     os_delay_us(2 * 1000 * 1000);
     system_os_post(user_procTaskPrio, 0, 0 );
@@ -40,12 +50,15 @@ user_init()
     //Set station mode
     wifi_set_opmode(STATION_MODE);
 
-
     uart_div_modify( 0, UART_CLK_FREQ / ( 115200 ) );
-    //Start os task
-    if (wifi_get_macaddr(STATION_IF, mac_addr) != true) {
+    if (wifi_get_macaddr(STATION_IF, original_mac_addr) != true) {
         os_printf("Failed to get the MAC address\n");
     } 
+
+    if (wifi_set_macaddr(STATION_IF, new_mac_addr) != true) {
+        os_printf("Failed to set the MAC address\n");
+    }
+    //Start os task
     system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 
     system_os_post(user_procTaskPrio, 0, 0 );
