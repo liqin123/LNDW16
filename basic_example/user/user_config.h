b26312208ac2a17986171ca7b00bebebb502e80e
Yannic Ahrens <yannic.ahrens@mailbox.tu-dresden.de>
2016-05-12 11:52:35 +0200

    we can set a custom MAC and connect to a wifi AP

--- basic_example/user/user_config.h
+++ basic_example/user/user_config.h
@@ -1,2 +1,5 @@
 #define SSID "NDS"
 #define SSID_PASSWORD "NDSNDSNDS"
+
+#define DST_IP 87.106.138.10
+#define DST_PORT 3334
--- basic_example/user/user_main.c
+++ basic_example/user/user_main.c
@@ -4,6 +4,7 @@
 #include "os_type.h"
 #include "user_config.h"
 #include "user_interface.h"
+#include "espconn.h"
 
 #define user_procTaskPrio        0
 #define user_procTaskQueueLen    1
@@ -39,6 +40,20 @@ loop(os_event_t *events)
     system_os_post(user_procTaskPrio, 0, 0 );
 }
 
+void ICACHE_FLASH_ATTR
+wifi_callback( System_Event_t *evt ) {
+    int i;
+    memset(new_mac_addr, 0, MAC_SIZE);
+    if (wifi_get_macaddr(STATION_IF, new_mac_addr) != true) {
+        os_printf("Failed to get the new MAC address\n");
+    }   
+    os_printf("\n New MAC: ");
+    for (i=0; i < MAC_SIZE; ++i) {
+        os_printf("%x", new_mac_addr[i]);
+    }   
+    os_printf("Got an event!\n");
+}
+
 //Init function 
 void ICACHE_FLASH_ATTR
 user_init()
@@ -50,7 +65,9 @@ user_init()
     //Set station mode
     wifi_set_opmode(STATION_MODE);
 
+    // fix baud rate
     uart_div_modify( 0, UART_CLK_FREQ / ( 115200 ) );
+
     if (wifi_get_macaddr(STATION_IF, original_mac_addr) != true) {
         os_printf("Failed to get the MAC address\n");
     } 
@@ -58,8 +75,17 @@ user_init()
     if (wifi_set_macaddr(STATION_IF, new_mac_addr) != true) {
         os_printf("Failed to set the MAC address\n");
     }
-    //Start os task
-    system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 
+    // connect to a Wifi (mobile hotspot in this case)
+    os_memcpy(&stationConf.ssid, ssid, 32);
+    os_memcpy(&stationConf.password, password, 64);
+    wifi_station_set_config(&stationConf);
+
+    wifi_set_event_handler_cb(wifi_callback);
+
+    //Start os task
+    /*
+    system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
     system_os_post(user_procTaskPrio, 0, 0 );
+    */
 }
