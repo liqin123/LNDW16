4f21134712d2eef435a84a44f219bee69a55d5b4
Yannic Ahrens <yannic.ahrens@mailbox.tu-dresden.de>
2016-05-12 10:04:40 +0200

    print the mac every two seconds

--- basic_example/user/user_main.c
+++ basic_example/user/user_main.c
@@ -10,12 +10,22 @@
 os_event_t    user_procTaskQueue[user_procTaskQueueLen];
 static void loop(os_event_t *events);
 
+#define MAC_SIZE 6
+uint8 mac_addr [MAC_SIZE] = {0, 0, 0, 0, 0, 0};
+
+// wifi: ESP has two "interfaces": one when acting as a station and another when it's acting as an AP
+
+
 //Main code function
 static void ICACHE_FLASH_ATTR
 loop(os_event_t *events)
 {
-    os_printf("Hello\n\r");
-    os_delay_us(10000);
+    int i;
+    os_printf("\n MAC: ");
+    for (i=0; i < MAC_SIZE; ++i) {
+        os_printf("%x", mac_addr[i]);
+    }
+    os_delay_us(2 * 1000 * 1000);
     system_os_post(user_procTaskPrio, 0, 0 );
 }
 
@@ -28,15 +38,14 @@ user_init()
     struct station_config stationConf;
 
     //Set station mode
-    wifi_set_opmode( 0x1 );
+    wifi_set_opmode(STATION_MODE);
 
-    //Set ap settings
-    os_memcpy(&stationConf.ssid, ssid, 32);
-    os_memcpy(&stationConf.password, password, 64);
-    wifi_station_set_config(&stationConf);
 
     uart_div_modify( 0, UART_CLK_FREQ / ( 115200 ) );
     //Start os task
+    if (wifi_get_macaddr(STATION_IF, mac_addr) != true) {
+        os_printf("Failed to get the MAC address\n");
+    } 
     system_os_task(loop, user_procTaskPrio,user_procTaskQueue, user_procTaskQueueLen);
 
     system_os_post(user_procTaskPrio, 0, 0 );
